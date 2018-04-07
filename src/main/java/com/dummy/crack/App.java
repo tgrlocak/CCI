6a464c3003be35f98eb1dfd765b7cba5256e305b
Tugrul Ocak <tugrul.ocak@obss.com.tr>
2018-04-07 22:55:17 +0300

    First

---
+++ src/main/java/com/dummy/crack/App.java
@@ -0,0 +1,13 @@
+package com.dummy.crack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
